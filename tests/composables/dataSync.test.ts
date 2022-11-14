f6cb584480d4556bba0b2d150a50f1ecf7427d08
brofrain <downloading130@gmail.com>
2022-11-14 19:08:20 +0100

    test: add data sync tests

--- tests/composables/dataSync.test.ts
+++ tests/composables/dataSync.test.ts
@@ -1,4 +1,8 @@
-import { expectUuid, expectDateString } from '~test-utils';
+import {
+  expectUuid,
+  expectDateString,
+  flushDelayedPromises,
+} from '~test-utils';
 
 import { useSynchronizableArray } from '~/composables/dataSync';
 
@@ -140,5 +144,84 @@ describe('useSynchronizableArray', () => {
       expectExampleDataItem(testItem3),
       expectExampleDataItem(testItem2),
     ]);
+  });
+
+  it('it should sync added items', async () => {
+    vi.useFakeTimers();
+    const uploader = vi.fn(() => Promise.resolve([]));
+
+    const { push } = $(
+      useSynchronizableArray<ExampleDataItem>(
+        'some-data',
+        vi.fn(() => Promise.resolve([])),
+        uploader,
+        vi.fn()
+      )
+    );
+
+    push(testItem1);
+    await flushDelayedPromises();
+    expect(uploader).toHaveBeenCalledOnce();
+    expect(uploader).toHaveBeenCalledWith([expectExampleDataItem(testItem1)]);
+
+    push(testItem2);
+    await flushDelayedPromises();
+    expect(uploader).toHaveBeenCalledTimes(2);
+    expect(uploader).toHaveBeenCalledWith([expectExampleDataItem(testItem2)]);
+
+    push(testItem3);
+    await flushDelayedPromises();
+    expect(uploader).toHaveBeenCalledTimes(3);
+    expect(uploader).toHaveBeenCalledWith([expectExampleDataItem(testItem3)]);
+  });
+
+  it('it should sync deleted items', async () => {
+    vi.useFakeTimers();
+    const deleter = vi.fn(() => Promise.resolve([]));
+
+    const { state, push, deleteById } = $(
+      useSynchronizableArray<ExampleDataItem>(
+        'some-data',
+        vi.fn(() => Promise.resolve([])),
+        vi.fn(),
+        deleter
+      )
+    );
+
+    push(testItem1);
+    push(testItem2);
+    push(testItem3);
+
+    expect(state).toStrictEqual([
+      expectExampleDataItem(testItem1),
+      expectExampleDataItem(testItem2),
+      expectExampleDataItem(testItem3),
+    ]);
+
+    await flushDelayedPromises();
+
+    {
+      const id = state[0].id;
+      deleteById(id);
+      await flushDelayedPromises();
+      expect(deleter).toHaveBeenCalledOnce();
+      expect(deleter).toHaveBeenCalledWith([id]);
+    }
+
+    {
+      const id = state[0].id;
+      deleteById(id);
+      await flushDelayedPromises();
+      expect(deleter).toHaveBeenCalledTimes(2);
+      expect(deleter).toHaveBeenCalledWith([id]);
+    }
+
+    {
+      const id = state[0].id;
+      deleteById(id);
+      await flushDelayedPromises();
+      expect(deleter).toHaveBeenCalledTimes(3);
+      expect(deleter).toHaveBeenCalledWith([id]);
+    }
   });
 });
