e237923ab86c37d0377ce1d963c287d702c8a326
brofrain <downloading130@gmail.com>
2022-11-29 19:39:31 +0100

    feat: nested nav draft

--- src/components/&layouts/_default/TheMobileNav.vue
+++ src/components/&layouts/_default/TheMobileNav.vue
@@ -4,40 +4,49 @@ import { useNav } from './useNav';
 const { t } = useI18n();
 const route = useRoute();
 const router = useRouter();
-const { navigableRoutes } = useNav();
+const { navigableRoutes, currentNavigableRoute } = useNav();
 </script>
 
 <template>
-  <div _flex _justify-center _gap1 _bg="primary-contrast/30" _p="t1 x1">
-    <div
-      v-for="{ path, meta: { navIcon, title } } in navigableRoutes"
-      :key="path"
-      _grow
-      _flex="~ col"
-      _items-center
-      _gap="[0.1rem]"
-      _rounded
-      _p="t2"
-      _relative
-      _cursor-pointer
-      _bg="#fff/60 hover:accent"
-      _text="#222 hover:accent-contrast"
-      _transition-all
-      @click="router.push(path)"
-    >
-      <div :class="`icon-${navIcon}`" />
-      <div _text-sm>
-        {{ t(title) }}
-      </div>
+  <div _flex="~ col">
+    <BaseFadeTransition>
+      <div v-if="currentNavigableRoute?.children?.length">it has kiddos</div>
+    </BaseFadeTransition>
 
-      <BaseFadeTransition>
+    <div _flex _justify-center _gap1 _bg="primary-contrast/30" _p="t1 x1">
+      <div
+        v-for="{ path, meta: { navIcon, title } } in navigableRoutes"
+        :key="path"
+        _grow
+      >
         <div
-          v-if="route.path === path"
-          _cover
-          _pointer-events-none
-          _border="2 accent"
-        />
-      </BaseFadeTransition>
+          _flex="~ col"
+          _items-center
+          _gap="[0.1rem]"
+          _rounded
+          _p="t2"
+          _relative
+          _cursor-pointer
+          _bg="#fff/60 hover:accent"
+          _text="#222 hover:accent-contrast"
+          _transition-all
+          @click="router.push(path)"
+        >
+          <div :class="`icon-${navIcon}`" />
+          <div _text-sm>
+            {{ t(title) }}
+          </div>
+
+          <BaseFadeTransition>
+            <div
+              v-if="route.path === path"
+              _cover
+              _pointer-events-none
+              _border="2 accent"
+            />
+          </BaseFadeTransition>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/components/&layouts/_default/useNav.ts
+++ src/components/&layouts/_default/useNav.ts
@@ -1,22 +1,74 @@
-import type { NavigableRoute } from '~/types/nav';
+import type {
+  RouteMeta,
+  RouteRecordNormalized,
+  RouteRecordRaw,
+} from 'vue-router';
+import type { LocaleKey } from '~/types/i18n';
+
+interface NavigableRouteRecordMeta extends RouteMeta {
+  nav: true;
+  navOrder: number;
+  navIcon: string;
+  title: LocaleKey;
+}
+
+interface NavigableRouteRecordNormalized extends RouteRecordNormalized {
+  meta: NavigableRouteRecordMeta;
+}
+
+type NavigableRouteRecordRaw = RouteRecordRaw & {
+  meta: NavigableRouteRecordMeta;
+};
+
+type NavigableRouteRecord =
+  | NavigableRouteRecordNormalized
+  | NavigableRouteRecordRaw;
+
+type NavigableRoutes = (NavigableRouteRecordNormalized & {
+  children: NavigableRouteRecordRaw[];
+})[];
+
+// @ts-expect-error TS is stupid
+const isNavigableRouteRecord: {
+  (route: RouteRecordNormalized): route is NavigableRouteRecordNormalized;
+  (route: RouteRecordRaw): route is NavigableRouteRecordRaw;
+} = (route) =>
+  !!(
+    route.meta?.title &&
+    route.meta.nav &&
+    isNum(route.meta.navOrder) &&
+    route.meta.navIcon
+  );
+
+const routeSortCompareCb = (a: NavigableRouteRecord, b: NavigableRouteRecord) =>
+  a.meta.navOrder - b.meta.navOrder;
 
 export const useNav = () => {
   const router = useRouter();
+  const route = useRoute();
 
-  const navigableRoutes = computed<NavigableRoute[]>(() =>
-    router
-      .getRoutes()
-      .filter(
-        (route): route is NavigableRoute =>
-          !!(
-            route.meta.title &&
-            route.meta.nav &&
-            isNum(route.meta.navOrder) &&
-            route.meta.navIcon
-          )
-      )
-      .sort((a, b) => a.meta.navOrder - b.meta.navOrder)
+  const navigableRoutes = $computed<NavigableRoutes>(() =>
+    klona(router.getRoutes())
+      .filter<NavigableRouteRecordNormalized>(isNavigableRouteRecord)
+      .sort(routeSortCompareCb)
+      .map(({ children, ...route }) => ({
+        ...route,
+        children: children
+          .filter(isNavigableRouteRecord)
+          .sort(routeSortCompareCb),
+      }))
   );
 
-  return { navigableRoutes };
+  const _navigableRouteRecordsToSearch = $computed(() => [
+    ...navigableRoutes,
+    ...navigableRoutes.flatMap(({ children }) => children),
+  ]);
+
+  const currentNavigableRoute = $computed<NavigableRouteRecord | null>(
+    () =>
+      _navigableRouteRecordsToSearch.find(({ path }) => path === route.path) ||
+      null
+  );
+
+  return $$({ navigableRoutes, currentNavigableRoute });
 };
